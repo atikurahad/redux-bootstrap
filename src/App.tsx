7316f80ce6e614867c87f40d5a99c7e1d41efbbe
unknown <atikurahad@gmail.com>
2025-01-03 00:35:35 +0600

    implement redux store and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, reset,} from "./redux/features/counter/counterSlice";
+import { RootState } from "./redux/store";
+
 function App() {
+  const dispatch = useDispatch();
+  const {count} = useSelector((state:RootState)=>state.counter);
 
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  const handleIncrement = (amount:number) => {
+    dispatch(increment(amount));
+  };
+  const handleReset = () => {
+    dispatch(reset());
+  };
+  return (
+    <>
+      <h1>Counter with redux</h1>
+      <div className="count">{count}</div>
 
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+      <div className="middle">
+        <button onClick={handleDecrement} className="btn">Decrement</button>
+        <button onClick={()=>handleIncrement(5)} className="btn">Increment 5</button>
+        <button onClick={handleReset} className="btn">Reset</button>
+
+        <button onClick={()=>handleIncrement(1)} className="btn">Increment</button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,15 @@
 // store.ts
 
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice"
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     // Add your reducers here
+    counter : counterReducer
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
